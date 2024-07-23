265354cce759fdc5eaaad8e9b26216efa4509242
uxnocode-dev <136633676+uxnocode-dev@users.noreply.github.com>
2024-07-23 00:45:32 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -38,7 +38,7 @@ function hideOverlay() {
         renderer: 'svg', // ou 'canvas' se preferir
         loop: true,
         autoplay: true,
-        path: 'https://uxnocode-dev.github.io/preloaderJSLottie/JVGIsylS3F-3.json',
+        path: 'https://uxnocode-dev.github.io/preloaderJSLottie/Animation - 1721706046738.json',
     });
 }
 
